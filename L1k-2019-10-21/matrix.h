dd3ee0e4cd72d8705c0413dce4eed9ce6a0c905c
Smak80 <smak-80@yandex.ru>
2019-10-31 15:35:21 +0300

    Лекция 1к. 2019-10-31

--- L1k-2019-10-21/matrix.h
+++ L1k-2019-10-21/matrix.h
@@ -2,6 +2,6 @@
 void show_matrix(int** matrix, int row_count, int col_count);
 void create_matrix(int**& matrix, int row_count, int col_count);
 void fill_matrix(int** matrix, int row_count, int col_count, int fill_type = 0);
-void fill_matrix(int** matrix, int row_count, int col_count);
+//void fill_matrix(int** matrix, int row_count, int col_count);
 void clear_matrix(int**& matrix, int row_count);
-void clear_matrix(double**& matrix, int row_count);
+//void clear_matrix(double**& matrix, int row_count);
